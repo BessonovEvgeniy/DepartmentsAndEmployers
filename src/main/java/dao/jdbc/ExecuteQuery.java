2c637f7615851e1c7a6bb92d17d1dcfcd8c6e2aa
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2017-03-05 18:35:22 +0200

    getAll Departments is works

--- src/main/java/dao/jdbc/ExecuteQuery.java
+++ src/main/java/dao/jdbc/ExecuteQuery.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 public class ExecuteQuery {
 
     static final String JDBC_DRIVER = "org.postgresql.Driver";
-    static final String DB_URL = "jdbc:postgresql://localhost:5432/deb_emp_db";
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/dep_emp_db";
 
     //Database credentials
     static final String USER = "postgres";
@@ -15,9 +15,9 @@ public class ExecuteQuery {
     Connection connection = null;
     Statement statement = null;
 
-    private Connection getConnection() throws SQLException, ClassNotFoundException{
+    private Connection getConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
 
-        Class.forName(JDBC_DRIVER);
+        Class.forName(JDBC_DRIVER).newInstance();
 
         Properties connectionsProps = new Properties();
 
@@ -27,7 +27,7 @@ public class ExecuteQuery {
         return DriverManager.getConnection(DB_URL,connectionsProps);
     }
 
-    public ResultSet createQuery(String sql) throws SQLException, ClassNotFoundException{
+    public ResultSet createQuery(String sql) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         connection = this.getConnection();
 
